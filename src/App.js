d93fbbc15ecd15d907b70e88e9d73467634ceca8
AmiRobinsonMuto <amimuto1002@gmail.com>
2023-02-26 19:53:20 +0900

    Update

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
 import React from "react";
 import Counter from "./Counter";
 
-function App() {
-  return (
-    <div className="App">
-      <Counter />
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      mount: true,
+    };
+
+    this.mountCounter = () => this.setState({ mount: true });
+    this.unmountCounter = () => this.setState({ mount: false });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <button onClick={this.mountCounter} disabled={this.state.mount}>
+          Mount
+        </button>
+        <button onClick={this.unmountCounter} disabled={!this.state.mount}>
+          Unmount
+        </button>
+        {this.state.mount ? <Counter /> : null}
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/Counter.js
+++ src/Counter.js
@@ -1,5 +1,43 @@
 import React from "react";
 
-class Counter extends React.Component {}
+class Counter extends React.Component {
+  constructor(props) {
+    console.log("Constructor");
+    super(props);
+    this.state = {
+      count: 0,
+    };
+
+    this.increment = () => this.setState({ count: this.state.count + 1 });
+    this.decrement = () => this.setState({ count: this.state.count - 1 });
+  }
+
+  componentDidMount() {
+    console.log("Component Did Mount");
+    console.log("---------------------------");
+  }
+
+  render() {
+    console.log("Render");
+
+    return (
+      <div>
+        <button onClick={this.increment}>Increment</button>
+        <button onClick={this.decrement}>Decrement</button>
+        <div>Counter: {this.state.count}</div>
+      </div>
+    );
+  }
+
+  componentDidUpdate(prevProps, prevState, snapshot) {
+    console.log("Component Did Update");
+    console.log("---------------------------");
+  }
+
+  componentWillUnmount() {
+    console.log("Component Will Unmount");
+    console.log("---------------------------");
+  }
+}
 
 export default Counter;
